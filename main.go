c0a254da2ef821ee129937dcfdc5ef524b317dd1
Russell Boley <raboley@gmail.com>
2020-09-07 11:08:35 -0700

    Added mocking example to mock time.Sleep() with a testable interface. It seems to work, but we don't verify the order of the actions so it could sleep for 4 seconds then do all 4 at once.

--- main.go
+++ main.go
@@ -3,21 +3,32 @@ package main
 import (
 	"fmt"
 	"github.com/raboley/go-by-tests/injection/injection"
+	"github.com/raboley/go-by-tests/injection/mocking"
 	"log"
 	"net/http"
 	"os"
 )
 
 func main() {
-	// io.writer can be used for stdout to terminal
-	writer := os.Stdout
-	if err := injection.Greet(writer, "Russell"); err != nil {
-		log.Fatalf("Got an error trying to make a greeting! error: %q", err)
+	var packageToRun string
+	if len(os.Args) > 1 {
+		packageToRun = "injection"
+	}
+	if packageToRun == "injection" {
+		// io.writer can be used for stdout to terminal
+		writer := os.Stdout
+		if err := injection.Greet(writer, "Russell"); err != nil {
+			log.Fatalf("Got an error trying to make a greeting! error: %q", err)
+		}
+
+		// io.writer can also be used for http requests
+		port := "5000"
+		if err := http.ListenAndServe(fmt.Sprintf(":%s", port), http.HandlerFunc(injection.MyGreeterHandler)); err != nil {
+			log.Fatalf("Error trying to serve over port %s", port)
+		}
 	}
 
-	// io.writer can also be used for http requests
-	port := "5000"
-	if err := http.ListenAndServe(fmt.Sprintf(":%s", port), http.HandlerFunc(injection.MyGreeterHandler)); err != nil {
-		log.Fatalf("Error trying to serve over port %s", port)
-	}
+	sleeper := &mocking.DefaultSleeper{}
+	mocking.Countdown(os.Stdout, sleeper)
+
 }
---
+++ mocking/mocking.go
@@ -0,0 +1,30 @@
+package mocking
+
+import (
+	"fmt"
+	"io"
+	"time"
+)
+
+func Countdown(writer io.Writer, sleeper Sleeper) {
+	for i := countdownStart; i > 0; i-- {
+		sleeper.Sleep()
+		_, _ = fmt.Fprintln(writer, i)
+	}
+
+	sleeper.Sleep()
+	_, _ = fmt.Fprint(writer, finalWord)
+}
+
+type Sleeper interface {
+	Sleep()
+}
+
+type DefaultSleeper struct{}
+
+func (s *DefaultSleeper) Sleep() {
+	time.Sleep(1 * time.Second)
+}
+
+const finalWord = "Go!"
+const countdownStart = 3
